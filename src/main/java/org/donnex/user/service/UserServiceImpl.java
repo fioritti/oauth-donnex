6dee3c1bdce8b9eae2af28fb1ba36a99181e505d
fioritti <guileme13@gmail.com>
2017-08-18 18:43:53 -0300

    alterações jwt

--- src/main/java/org/donnex/user/service/UserServiceImpl.java
+++ src/main/java/org/donnex/user/service/UserServiceImpl.java
@@ -3,42 +3,37 @@ package org.donnex.user.service;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import javax.annotation.PostConstruct;
-
 import org.donnex.user.model.Role;
 import org.donnex.user.model.User;
 import org.donnex.user.repository.RoleRepository;
 import org.donnex.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserServiceImpl {
 	
-	@PostConstruct
-	public void init() {
-		Role role = new Role();
-		role.setRole("ADMIN");
-		roleRepository.save(role);
-	}
+//	@PostConstruct
+//	public void init() {
+//		Role role = new Role();
+//		role.setRole("ADMIN");
+//		roleRepository.save(role);
+//	}
 
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
     private RoleRepository roleRepository;
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private PasswordEncoder passwordencoder;
 	
 	public User findUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 
 	public void saveUser(User user) {
-		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setPassword(passwordencoder.encode(user.getPassword()));
         user.setActive(1);
         Role userRole = roleRepository.findByRole("ADMIN");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
